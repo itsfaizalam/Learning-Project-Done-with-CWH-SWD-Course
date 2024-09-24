28468b8d859277e82d375843aacb26c737d81a23
Faiz Alam <alam.faiz3@gmail.com>
2024-09-24 20:09:42 +0530

    video 83 solution done

---
+++ 01-student-house-solution.js
@@ -0,0 +1,23 @@
+// Solution the question 1.
+let students = ["Faiz", "Shama", "Shivani", "Shivangi", "Annapurna", "Shubham", "Krishnanendu", "Ravindranathan", "Shivesh", "kaif", "Emanuel"]
+
+let houses = []
+for (const student of students) {
+    if(student.length < 6){
+        houses.push("Red")
+    }
+    else if(student.length < 8){
+        houses.push("Blue")
+    }
+    else if(student.length < 12){
+        houses.push("Green")
+    }
+    else{
+        houses.push("Yellow")
+    }
+}
+
+console.log("House Assignments:");
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i]} : ${houses[i]} house`);
+}
